0eef01ce21d2e9cd96144ea71f96cd6de2798bdf
bluelovers <codelovers@users.sourceforge.net>
2016-04-07 00:16:17 +0800

    mocha (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	test/index.js
    
    [ M] modified - Changes not staged for commit:
    
    	package.json
    	src/index.js

---
+++ test/index.js
@@ -0,0 +1,92 @@
+/**
+ * Created by user on 2016/4/6.
+ */
+"use strict";
+
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+var should = chai.should;
+
+var path = require('path');
+var upath = require('../src/index.js');
+
+function path_relative(to)
+{
+	return path.relative(__dirname + '/../', to);
+}
+
+function lazyassert()
+{
+
+}
+
+describe(path_relative(__filename), function ()
+{
+	describe('path.normalize(path)', function ()
+	{
+		var _map = {
+			'c:/windows/nodejs/path': 'c:/windows/nodejs/path',
+			'c:/windows/../nodejs/path': 'c:/nodejs/path',
+			'c:\\windows\\nodejs\\path': 'c:/windows/nodejs/path',
+			'c:\\windows\\..\\nodejs\\path': 'c:/nodejs/path',
+			'//windows\\unix/mixed': '/windows/unix/mixed',
+			'\\windows//unix/mixed': '/windows/unix/mixed',
+			'////\\windows\\..\\unix/mixed/': '/unix/mixed/'
+		};
+
+		for (let p in _map)
+		{
+			it(`${ p } => ${ _map[p] }`, function () {
+				assert.equal(upath.normalize(p), _map[p]);
+			});
+		}
+	});
+
+	describe('path.join(paths...)', function ()
+	{
+		var _map = [
+			[
+				['some/nodejs/deep', '../path'],
+				'some/nodejs/path',
+			],
+			[
+				['some/nodejs\\windows', '../path'],
+				'some/nodejs/path',
+			],
+			[
+				['some\\windows\\only', '..\\path'],
+				'some/windows/path',
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.join.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+
+	describe('path.toUnix(path)', function ()
+	{
+		var _map = [
+			[
+				['.//windows\//unix//mixed////'],
+				'./windows/unix/mixed/',
+			],
+			[
+				['..///windows\\..\\unix/mixed'],
+				'../windows/../unix/mixed',
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.toUnix.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+});
