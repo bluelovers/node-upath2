1745207f5f5d11b7c9e2f82fd7f3a960e5058372
bluelovers <codelovers@users.sourceforge.net>
2016-04-07 03:43:22 +0800

    add posix, but not sure work right (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	test/posix.js
    
    [ M] modified - Changes not staged for commit:
    
    	src/index.js

---
+++ test/posix.js
@@ -0,0 +1,195 @@
+/**
+ * Created by user on 2016/4/7.
+ *
+ * https://github.com/jden/node-path-posix/blob/master/test/test-path.js
+ */
+
+"use strict";
+
+var chai = require('chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+var should = chai.should;
+
+var path = require('path');
+var upath = require('../src/index.js').posix;
+
+function path_relative(to)
+{
+	return path.relative(__dirname + '/../', to);
+}
+
+function lazyassert()
+{
+
+}
+
+describe(path_relative(__filename), function ()
+{
+	describe('path.normalize(path)', function ()
+	{
+		var _map = {
+			'c:/windows/nodejs/path': 'c:/windows/nodejs/path',
+			'c:/windows/../nodejs/path': 'c:/nodejs/path',
+			'c:\\windows\\nodejs\\path': 'c:/windows/nodejs/path',
+			'c:\\windows\\..\\nodejs\\path': 'c:/nodejs/path',
+			'//windows\\unix/mixed': '/windows/unix/mixed',
+			'\\windows//unix/mixed': '/windows/unix/mixed',
+			'\\\\windows//unix/mixed': '/windows/unix/mixed',
+			'////\\windows\\..\\unix/mixed/': '/unix/mixed/',
+
+			'////\\windows\\////\\////\\..\\////\\////\\unix/////\\mixed/': '/unix/mixed/',
+
+			'windows\\unix/mixed/': 'windows/unix/mixed/'
+		};
+
+		for (let p in _map)
+		{
+			it(`${ p } => ${ _map[p] }`, function () {
+				assert.equal(upath.normalize(p), _map[p]);
+			});
+		}
+	});
+
+	describe('path.join(paths...)', function ()
+	{
+		var _map = [
+			[
+				['some/nodejs/deep', '../path'],
+				'some/nodejs/path',
+			],
+			[
+				['some/nodejs\\windows', '../path'],
+				'some/nodejs/path',
+			],
+			[
+				['some\\windows\\only', '..\\path'],
+				'some/windows/path',
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.join.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+
+	describe('path.formatify(path)', function ()
+	{
+		var _map = [
+			[
+				['.//windows\//unix//mixed////'],
+				'./windows/unix/mixed/',
+			],
+			[
+				['..///windows\\..\\unix/mixed'],
+				'../windows/../unix/mixed',
+			],
+			[
+				['..///\\\\\\\\\\\\\\\//////////////////windows\\\\\\\\\\\\\\\//////////////////\\..\\unix/mixed'],
+				'../windows/../unix/mixed',
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.formatify.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+
+	describe('path.isAbsolute(path)', function ()
+	{
+		var _map = [
+			[
+				['D:\\Users\\Documents\\The Project\\JetBrains\\untitled\\package.json'],
+				false,
+			],
+			[
+				['\\untitled\\package.json'],
+				true,
+			],
+			[
+				['/home/foo'],
+				true,
+			],
+			[
+				['/home/foo/..'],
+				true,
+			],
+			[
+				['bar/'],
+				false,
+			],
+			[
+				['./baz'],
+				false,
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.isAbsolute.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+
+	describe('path.septrim(path)', function ()
+	{
+		var _map = [
+			[
+				['./'],
+				'.',
+			],
+			[
+				['./../'],
+				'..',
+			],
+			[
+				['./../dep/'],
+				'../dep',
+			],
+			[
+				['path//dep\\'],
+				'path/dep',
+			],
+			[
+				['.//windows\\unix/mixed/'],
+				'./windows/unix/mixed',
+			],
+			[
+				['./././././/windows/././././\\unix/././././mixed/'],
+				'./windows/unix/mixed',
+			],
+			[
+				['././././'],
+				'.',
+			],
+			[
+				['./././.././././.'],
+				'..',
+			],
+			[
+				['././.././dep/'],
+				'../dep',
+			],
+			[
+				['path//dep\\'],
+				'path/dep',
+			],
+		];
+
+		for (let i in _map)
+		{
+			it(`${ _map[i][0] } => ${ _map[i][1] }`, function () {
+				assert.equal(upath.septrim.apply(upath, _map[i][0]), _map[i][1]);
+			});
+		}
+	});
+
+});
